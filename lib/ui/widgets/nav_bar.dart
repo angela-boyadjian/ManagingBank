2587b26ffade44f1e41a419093f00d9828ed881d
angela-boyadjian <boyadjian.angela@gmail.com>
2020-12-07 13:20:42 +0100

    feat: navbar

--- lib/ui/widgets/nav_bar.dart
+++ lib/ui/widgets/nav_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ff_navigation_bar/ff_navigation_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:mimi/constants/constants.dart';
 
