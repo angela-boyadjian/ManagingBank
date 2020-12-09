7a773029a8a05cfc0fab5e47c458e33059711abf
angela-boyadjian <boyadjian.angela@gmail.com>
2020-12-09 20:37:55 +0100

    feat(front): result dropdown

--- lib/ui/screens/results/results_screen.dart
+++ lib/ui/screens/results/results_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mimi/ui/widgets/custom_app_bar.dart';
+import 'package:mimi/ui/widgets/custom_drop_down.dart';
 
 class ResultsScreen extends StatefulWidget {
   ResultsScreen({Key key}) : super(key: key);
@@ -8,19 +10,74 @@ class ResultsScreen extends StatefulWidget {
 }
 
 class _ResultsScreenState extends State<ResultsScreen> {
+  final List<FavouriteFoodModel> _favouriteFoodModelList = [
+    FavouriteFoodModel(foodName: 'Ce mois', calories: 161),
+    FavouriteFoodModel(foodName: 'Ce trimestre', calories: 220),
+    FavouriteFoodModel(foodName: 'Cette année', calories: 208),
+  ];
+  FavouriteFoodModel _favouriteFoodModel = FavouriteFoodModel();
+  List<DropdownMenuItem<FavouriteFoodModel>> _favouriteFoodModelDropdownList;
+
+  @override
+  void initState() {
+    _favouriteFoodModelDropdownList =
+        _buildFavouriteFoodModelDropdown(_favouriteFoodModelList);
+    _favouriteFoodModel = _favouriteFoodModelList[0];
+    super.initState();
+  }
+
+  List<DropdownMenuItem<FavouriteFoodModel>> _buildFavouriteFoodModelDropdown(
+      List favouriteFoodModelList) {
+    List<DropdownMenuItem<FavouriteFoodModel>> items = List();
+    for (FavouriteFoodModel favouriteFoodModel in favouriteFoodModelList) {
+      items.add(DropdownMenuItem(
+        value: favouriteFoodModel,
+        child: Text(favouriteFoodModel.foodName),
+      ));
+    }
+    return items;
+  }
+
+  _onChangeFavouriteFoodModelDropdown(FavouriteFoodModel favouriteFoodModel) {
+    setState(() {
+      _favouriteFoodModel = favouriteFoodModel;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'RESULTS SCREEN',
-          textAlign: TextAlign.center,
-          style: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(fontWeight: FontWeight.bold),
-        ),
+      appBar: CustomAppBar("Résultats"),
+      body: Column(
+        children: [
+          SizedBox(height: 30.0),
+          Flexible(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              child: CustomDropdown(
+                dropdownMenuItemList: _favouriteFoodModelDropdownList,
+                onChanged: _onChangeFavouriteFoodModelDropdown,
+                value: _favouriteFoodModel,
+                isEnabled: true,
+              ),
+            ),
+          ),
+          Spacer(flex: 2),
+          Spacer(flex: 2),
+          Spacer(flex: 2),
+          Spacer(flex: 2),
+        ],
       ),
     );
   }
+}
+
+class FavouriteFoodModel {
+  final String foodName;
+  final double calories;
+  FavouriteFoodModel({
+    this.foodName,
+    this.calories,
+  });
 }
---
+++ lib/ui/widgets/custom_drop_down.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomDropdown<T> extends StatelessWidget {
+  final List<DropdownMenuItem<T>> dropdownMenuItemList;
+  final ValueChanged<T> onChanged;
+  final T value;
+  final bool isEnabled;
+
+  CustomDropdown({
+    Key key,
+    @required this.dropdownMenuItemList,
+    @required this.onChanged,
+    @required this.value,
+    this.isEnabled = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      ignoring: !isEnabled,
+      child: Container(
+        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            border: Border.all(
+              color: Color(0xFFA9A9EF),
+              width: 1.5,
+            ),
+            color: isEnabled ? Colors.white : Colors.grey.withAlpha(100)),
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton(
+            focusColor: Color(0xFFEEEEFC),
+            isExpanded: true,
+            itemHeight: 50.0,
+            style: TextStyle(
+                fontSize: 15.0,
+                color: isEnabled ? Colors.black : Colors.grey[700]),
+            items: dropdownMenuItemList,
+            onChanged: onChanged,
+            value: value,
+          ),
+        ),
+      ),
+    );
+  }
+}
