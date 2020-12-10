26b78c66429bed4dea85a81760e6b824128666ad
angela-boyadjian <boyadjian.angela@gmail.com>
2020-12-10 16:46:23 +0100

    feat: add result cards

--- lib/ui/screens/results/results_screen.dart
+++ lib/ui/screens/results/results_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mimi/logic/cubit/drop_down/drop_down_cubit.dart';
-import 'package:mimi/ui/screens/results/widgets/prevision_card.dart';
 
 import 'package:mimi/ui/widgets/custom_app_bar.dart';
-import 'package:mimi/ui/widgets/drop_down/custom_drop_down.dart';
 import 'package:mimi/ui/widgets/drop_down/drop_down_model.dart';
+import 'package:mimi/ui/widgets/drop_down/custom_drop_down.dart';
+import 'package:mimi/logic/cubit/drop_down/drop_down_cubit.dart';
+import 'package:mimi/ui/screens/results/widgets/result_card.dart';
+import 'package:mimi/ui/screens/results/widgets/prevision_card.dart';
 
 class ResultsScreen extends StatefulWidget {
   ResultsScreen({Key key}) : super(key: key);
@@ -33,58 +34,79 @@ class _ResultsScreenState extends State<ResultsScreen> {
       body: Column(
         children: [
           SizedBox(height: 30.0),
-          Stack(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: PrevisionCard(
-                        title: "Prévisionnel recettes", amount: "56 476,98 €"),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10.0),
-                    child: PrevisionCard(
-                        title: "Prévisionnel dépenses", amount: "13 445,25 €"),
-                  ),
-                ],
-              ),
-              Center(
-                child: BlocProvider(
-                  create: (_) => DropDownCubit(items, 0),
-                  child: CustomDropDown(),
-                ),
-              ),
-              SizedBox(height: 10.0),
-            ],
-          ),
-          // SizedBox(height: 30.0),
-          // Center(
-          //   child: BlocProvider(
-          //     create: (_) => DropDownCubit(items, 0),
-          //     child: CustomDropDown(),
-          //   ),
-          // ),
-          // SizedBox(height: 10.0),
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+          // Stack(
           //   children: [
-          //     Padding(
-          //       padding: const EdgeInsets.only(left: 10.0),
-          //       child: PrevisionCard(
-          //           title: "Prévisionnel recettes", amount: "56 476,98 €"),
+          //     Row(
+          //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //       children: [
+          //         Padding(
+          //           padding: const EdgeInsets.only(left: 10.0),
+          //           child: PrevisionCard(
+          //               title: "Prévisionnel recettes", amount: "56 476,98 €"),
+          //         ),
+          //         Padding(
+          //           padding: const EdgeInsets.only(right: 10.0),
+          //           child: PrevisionCard(
+          //               title: "Prévisionnel dépenses", amount: "13 445,25 €"),
+          //         ),
+          //       ],
           //     ),
-          //     Padding(
-          //       padding: const EdgeInsets.only(right: 10.0),
-          //       child: PrevisionCard(
-          //           title: "Prévisionnel dépenses", amount: "13 445,25 €"),
+          //     Center(
+          //       child: BlocProvider(
+          //         create: (_) => DropDownCubit(items, 0),
+          //         child: CustomDropDown(),
+          //       ),
           //     ),
+          //     SizedBox(height: 10.0),
           //   ],
           // ),
-          Spacer(flex: 2),
-          Spacer(flex: 2),
-          Spacer(flex: 2),
+          // SizedBox(height: 30.0),
+          Center(
+            child: BlocProvider(
+              create: (_) => DropDownCubit(items, 0),
+              child: CustomDropDown(),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: PrevisionCard(
+                    title: "Prévisionnel recettes", amount: "56 476,98 €"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: PrevisionCard(
+                    title: "Prévisionnel dépenses", amount: "13 445,25 €"),
+              ),
+            ],
+          ),
+          SizedBox(height: 30.0),
+          // Spacer(flex: 1),
+          Flexible(
+            flex: 1,
+            child: ResultCard(
+                title: "Dépenses",
+                amount: "2 567,92 €",
+                color: Color(0xFFFDAF3A)),
+          ),
+          Flexible(
+            flex: 1,
+            child: ResultCard(
+                title: "Recettes",
+                amount: "10 546,12 €",
+                color: Color(0xFF48D73C)),
+          ),
+          Flexible(
+            flex: 1,
+            child: ResultCard(
+                title: "Trésorerie",
+                amount: "12 546,26 €",
+                color: Color(0xFF5353E0)),
+          ),
+          Spacer(flex: 1),
         ],
       ),
     );
---
+++ lib/ui/screens/results/widgets/result_card.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ResultCard extends StatefulWidget {
+  final String title;
+  final String amount;
+  final Color color;
+
+  ResultCard(
+      {Key key,
+      @required this.title,
+      @required this.amount,
+      @required this.color})
+      : super(key: key);
+
+  @override
+  _ResultCardState createState() => _ResultCardState();
+}
+
+class _ResultCardState extends State<ResultCard> {
+  Color color;
+
+  @override
+  void initState() {
+    setState(() {
+      color = widget.color;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1.0,
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: MediaQuery.of(context).size.width - 30.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        widget.title,
+                        style: Theme.of(context).textTheme.subtitle1.copyWith(
+                            fontSize: 14.0, fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    SizedBox(height: 5.0),
+                    Padding(
+                      padding: EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        widget.amount,
+                        style: Theme.of(context).textTheme.subtitle1.copyWith(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.w700,
+                            color: color),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              IconButton(
+                  icon: Icon(FontAwesomeIcons.chevronRight,
+                      color: Color.fromRGBO(36, 36, 97, 0.6)),
+                  onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
