2bf8cf07c105c5636c95857ab4b5ae22bda66154
angela-boyadjian <boyadjian.angela@gmail.com>
2020-12-17 16:58:18 +0100

    feat: create page

---
+++ lib/ui/screens/categories/categories_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatefulWidget {
+  CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  _CategoriesScreenState createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
